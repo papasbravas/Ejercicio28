b65426b95e88bf7f17a1a768dbc2583300a31af9
papasbravas <aeg2809@gmail.com>
2024-05-21 18:22:23 +0200

    Mi primer commit

---
+++ src/ejercicio28/Ejercicio28.java
@@ -0,0 +1,12 @@
+package ejercicio28;
+
+public class Ejercicio28 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
